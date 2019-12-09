b3fdbb06be13b3ece86bb1a99e32c68dba4c37bb
Richie Loughran <richie.loughran@ericsson.com>
2019-12-09 15:50:17 +0000

    05 - Removed WebClient and Reverted back to RestTemplate

--- movie-catalog-service/src/main/java/com/roccorichie/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/roccorichie/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
@@ -12,15 +11,6 @@ public class MovieCatalogServiceApplication {
     @Bean
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
-    }
-
-    @Bean
-    public WebClient.Builder getWebClientBuilder() {
-        // Reactive part of a Spring application - Spring WebFlux
-        // - Need to add to class path - Add dependency in to the pom and maven will grab it
-        // - asynchronous - give it me when you have it
-        // we will do other stuff while you are retrieving it (lambda)
-        return WebClient.builder();
     }
 
     public static void main(String[] args) {
--- movie-catalog-service/src/main/java/com/roccorichie/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/roccorichie/moviecatalogservice/resources/MovieCatalogResource.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,9 +21,6 @@ public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Autowired
-    private WebClient.Builder webClientBuilder;
-
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
@@ -35,23 +31,7 @@ public class MovieCatalogResource {
 
         return ratings.stream().map(rating -> {
             // RestTemplate -> Making a call to the API and un-marshalling the object
-            // Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-
-            // This builder will give an instance of movie
-            // Using builder pattern give me a webclient
-            // get method
-            // uri -> url
-            // Go do the fetch
-            // Whatever body you get back convert it to an instance of
-            // mono -> a promise that this will give you back this (movie) object asynchronously
-            // An empty container - when something is in it return it to the caller
-            // block - block execution until someone puts a movie into the container - synchronous
-            Movie movie = webClientBuilder.build()
-                    .get()
-                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
-                    .retrieve()
-                    .bodyToMono(Movie.class)
-                    .block();
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
 
             assert movie != null;
             return new CatalogItem(movie.getName(), "Awesome", rating.getRating());
