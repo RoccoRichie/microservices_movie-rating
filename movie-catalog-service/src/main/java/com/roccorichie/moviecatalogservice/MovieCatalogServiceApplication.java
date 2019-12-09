6ba5ca2e7fc1bfcae66207a4c4e3c0d732ac6eeb
Richie Loughran <richie.loughran@ericsson.com>
2019-12-09 15:02:33 +0000

    03 - Bean create for RestTemplate

--- movie-catalog-service/src/main/java/com/roccorichie/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/roccorichie/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,9 +2,23 @@ package com.roccorichie.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
+
+    // Create a Bean which will create a singleton version of the RestTemplate
+    // And make the instance be available for when it is required i.e
+    // take it out of getCatalog(), it is a singleton
+    // RestTemplates maps to this one instance - Content Delivery Injection
+    // A bean is telling Spring I have something which others may need (a producer)
+    // - Autowired says give me that something
+    // The name doesn't matter - the type does
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MovieCatalogServiceApplication.class, args);
--- movie-catalog-service/src/main/java/com/roccorichie/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/roccorichie/moviecatalogservice/resources/MovieCatalogResource.java
@@ -4,6 +4,7 @@ package com.roccorichie.moviecatalogservice.resources;
 import com.roccorichie.moviecatalogservice.models.CatalogItem;
 import com.roccorichie.moviecatalogservice.models.Movie;
 import com.roccorichie.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +19,16 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
+
+    // To use the RestTemplate bean create a property of the class.
+    // You need to Autowired it -> Inject me that Bean
+    @Autowired
+    private RestTemplate restTemplate;
     //Tell spring boot to treat this as an api -> give it an end point
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
         // make a call and get string response - Un-marshall it - create an object instance of it
-        RestTemplate restTemplate = new RestTemplate();
         // Get all rated movie Ids - Will hard code this first
         // Hardcoded for now - Assume this is the response (for a user) from hitting the Movie rating api
         List<Rating> ratings = Arrays.asList(
