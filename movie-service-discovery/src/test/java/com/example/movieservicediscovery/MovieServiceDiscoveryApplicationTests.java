43807ab91407385cc3a12b3d4ead36d3270813f8
Richie Loughran <richie.loughran@ericsson.com>
2019-12-11 11:58:53 +0000

    07a - Added empty discovery service Eureka server

---
+++ movie-service-discovery/src/test/java/com/example/movieservicediscovery/MovieServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieservicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
