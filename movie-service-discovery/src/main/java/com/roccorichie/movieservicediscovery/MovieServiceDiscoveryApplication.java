e0a848dcecf360e7af933f9ad30e6875a43d1418
Richie Loughran <richie.loughran@ericsson.com>
2019-12-12 11:09:16 +0000

    07b - Added empty discovery service Eureka server

--- movie-service-discovery/src/main/java/com/roccorichie/movieservicediscovery/MovieServiceDiscoveryApplication.java
+++ movie-service-discovery/src/main/java/com/roccorichie/movieservicediscovery/MovieServiceDiscoveryApplication.java
@@ -1,11 +1,9 @@
-package com.example.movieservicediscovery;
+package com.roccorichie.movieservicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class MovieServiceDiscoveryApplication {
 
     public static void main(String[] args) {
--- movie-service-discovery/src/test/java/com/roccorichie/movieservicediscovery/MovieServiceDiscoveryApplicationTests.java
+++ movie-service-discovery/src/test/java/com/roccorichie/movieservicediscovery/MovieServiceDiscoveryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.movieservicediscovery;
+package com.roccorichie.movieservicediscovery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
